74f17af29200d048184eaf2872fdf80e4584f158
AnhKhoa267 <trananhkhoa260702@gmail.com>
2022-11-30 20:44:22 +0700

    NNN tai lay 10k

---
+++ ExpenseTracker/ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
